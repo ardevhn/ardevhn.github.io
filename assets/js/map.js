830c5a645ac2e795b321eb7f6bf578a159ff4e63
Camilo Aguilar <camiloaguilar1@yahoo.com>
2015-08-29 21:35:26 -0600

    fixing contact

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,17 +1,18 @@
 var map;
 jQuery(document).ready(function(){
 
+//15.5155414,-87.9840303
     map = new GMaps({
         div: '#map',
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 15.5155414,
+        lng: -87.9840303,
     });
     map.addMarker({
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 15.5155414,
+        lng: -87.9840303,
         title: 'Address',      
         infoWindow: {
-            content: '<h5 class="title">DevStudio</h5><p><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="country-name">Country</span></p>'
+            content: '<h5 class="title">A &amp; R Development S. de R.L.</h5><p><span class="region">Res. Salamanca Bloque E Casa 6</span><br><span class="city">San Pedro Sula</span><br /><span class="postal-code">21101</span><br><span class="country-name">HN</span></p>'
         }
         
     });
